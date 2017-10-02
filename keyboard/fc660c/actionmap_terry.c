8ddf66a64d019e6c6ae4e555b926bd748ef1c63a
Terry <terry.h@outlook.com>
2017-10-02 18:18:23 -0500

    Added LShift + tap ':'

--- keyboard/fc660c/actionmap_terry.c
+++ keyboard/fc660c/actionmap_terry.c
@@ -15,7 +15,21 @@
 #define AC_BACK     ACTION_MODS_KEY(MOD_LALT, KC_LEFT)
 #define AC_FRWD     ACTION_MODS_KEY(MOD_LALT, KC_RIGHT)
 
-#define AC_CLN			ACTION_MODS_KEY(MOD_LSFT, KC_SCLN)
+#define AC_CLN		ACTION_MODS_KEY(MOD_LSFT, KC_SCLN)
+#define AC_LSH 		ACTION_FUNCTION_TAP(LSHIFT_LCLN)
+
+enum function_id {
+    LSHIFT_LCLN,
+};
+
+#ifdef KEYMAP_SECTION_ENABLE
+const action_t fn_actions[] __attribute__ ((section (".keymap.fn_actions"))) = {
+#else
+const action_t fn_actions[] PROGMEM = {
+#endif
+	ACTION_FUNCTION_TAP(LSHIFT_LCLN) //FN0
+};
+
 
 // emulates FC660C default keymap
 #ifdef KEYMAP_SECTION_ENABLE
@@ -27,15 +41,15 @@ const action_t actionmaps[][UNIMAP_ROWS][UNIMAP_COLS] PROGMEM = {
         GRV, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSPC,     PSCR,
         TAB, Q,   W,   F,   P,   G,   J,   L,   U,   Y,   SCLN,   LBRC,RBRC,BSLS,     DEL,
         ESC ,A,   R,   S,   T,   D,   H,   N,   E,   I,   O,   QUOT,     ENT,
-        LS_2,Z,   X,   C,   V,   B,   K,   M,   COMM,DOT, SLS6,          RS_2,   UP,
-        LCTL,  LGUI,LALT,          SPC5,                    L4, CLN ,L3,  LEFT,DOWN,RGHT
+        LSH, Z,   X,   C,   V,   B,   K,   M,   COMM,DOT, SLS6,          RS_2,   UP,
+        LCTL,  LGUI,LALT,          SPC5,                    L4, RALT ,L3,  LEFT,DOWN,RGHT
     ),
     [1] = KMAP(
         GRV, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSPC,     INS,
         TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSLS,     DEL,
         LCTL,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,     ENT,
         LSFT,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLS6,          RSFT,UP,
-        L4,  LGUI,LALT,          SPC5,                    RALT,TRNS,L4,  LEFT,DOWN,RGHT
+        L4,  LGUI,LALT,          SPC,                    RALT,TRNS,L4,  LEFT,DOWN,RGHT
     ),
     [2] = KMAP(
         GRV, TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,     TRNS,
@@ -82,5 +96,38 @@ void hook_layer_change(uint32_t layer_state)
         PORTB &= ~(1<<5);
     } else {
         PORTB |=  (1<<5);
+    }
+}
+
+void action_function(keyrecord_t *record, uint8_t id, uint8_t opt)
+{
+    switch (id) {
+        case LSHIFT_LCLN:
+			// Modified into LShift + tap ':'
+            // Shift parentheses example: LShft + tap '('
+            // http://stevelosh.com/blog/2012/10/a-modern-space-cadet/#shift-parentheses
+            // http://geekhack.org/index.php?topic=41989.msg1304899#msg1304899
+            if (record->event.pressed) {
+                if (record->tap.count > 0 && !record->tap.interrupted) {
+                    if (record->tap.interrupted) {
+                        register_mods(MOD_BIT(KC_LSHIFT));
+                    }
+                } else {
+                    register_mods(MOD_BIT(KC_LSHIFT));
+                }
+            } else {
+                if (record->tap.count > 0 && !(record->tap.interrupted)) {
+                    add_weak_mods(MOD_BIT(KC_LSHIFT));
+                    send_keyboard_report();
+                    register_code(KC_SCLN);
+                    unregister_code(KC_SCLN);
+                    del_weak_mods(MOD_BIT(KC_LSHIFT));
+                    send_keyboard_report();
+                    record->tap.count = 0;  // ad hoc: cancel tap
+                } else {
+                    unregister_mods(MOD_BIT(KC_LSHIFT));
+                }
+            }
+            break;
     }
 }
